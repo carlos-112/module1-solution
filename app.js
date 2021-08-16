e2321ab3a0b94b3addd6b8f94ceaaf32bccae41d
Carlos Franco <me@cfrco.com>
2021-08-15 22:29:06 -0700

    final

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
 (function () {
     'use strict';
-    
     angular.module('LunchBreak', [])
-    
     .controller('LunchCheckController', LunchCheckController);
-    
     LunchCheckController.$inject = ['$scope'];
-
     function LunchCheckController($scope) {
-
         $scope.clickIt = function(){
-
-            if ($scope.myPlaceHolder != null) {
-
+            if ($scope.myPlaceHolder != null && $scope.myPlaceHolder != "") {
             var menuItems = $scope.myPlaceHolder.replace(/\s+/g, '').split(",");
-            
             menuItems = menuItems.filter((a) => a);
-
             var count = menuItems.length;
-
                 if (count < 4){
                     $scope.myTextBox = "Enjoy!"
                 }
-
                 else {
                     $scope.myTextBox = "Too Much!"
                 }
-
             }
-
             else {
                $scope.myTextBox = "Please Enter Food!"
             }
-
         }
-
     } 
-    
 })();
-    
